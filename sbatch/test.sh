fa80a8741a51b0d74138150efd0e6cddc2ec0440
Pierce Wang <pierce.g.wang@gmail.com>
2023-10-20 15:38:36 -0400

    cleanup(models): remove unnecessary models

--- sbatch/test.sh
+++ sbatch/test.sh
@@ -11,6 +11,9 @@
 #SBATCH --mem-per-cpu=1gb        # The memory the job will use per cpu core.
 #SBATCH --gres=gpu
 
+module load cuda11.7/toolkit
+module load anaconda
+pip install --user torch==2.0.0+cu117 torchvision==0.15.1+cu117 torchaudio==2.0.1 --index-url https://download.pytorch.org/whl/cu117
 echo "Hello World"
 sleep 10
 date
